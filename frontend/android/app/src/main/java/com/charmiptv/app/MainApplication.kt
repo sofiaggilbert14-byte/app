1f09c38605995e858a027b9fd16837b0f2c7c0ab
sofiaggilbert14-byte <sofiaggilbert14@gmail.com>
2026-08-13 08:54:04 -0400

    Register RAM EPG package

--- frontend/android/app/src/main/java/com/charmiptv/app/MainApplication.kt
+++ frontend/android/app/src/main/java/com/charmiptv/app/MainApplication.kt
@@ -27,6 +27,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               add(TvRemotePackage())
               add(EpgNativePackage())
+              add(EpgRamPackage())
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
             }
