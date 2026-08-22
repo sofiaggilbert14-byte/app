0c8dcb8cdddbffd0a8fee43560e5703e5d5d2caf
floydleroybuchanan-collab <floydleroybuchanan@gmail.com>
2026-08-22 07:12:42 -0400

    feat: register native playback package

--- frontend/android/app/src/main/java/com/charmiptv/app/MainApplication.kt
+++ frontend/android/app/src/main/java/com/charmiptv/app/MainApplication.kt
@@ -26,6 +26,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               add(TvRemotePackage())
+              add(NativePlaybackPackage())
               add(EpgNativePackage())
               add(EpgRamPackage())
               add(NativeGuidePackage())
